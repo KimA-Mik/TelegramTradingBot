880510a8b442fd13fa62384cabece50de96e3404
KimA-Mik <ananim38@gmail.com>
2024-03-18 16:03:05 +0300

    Bump coroutines-core to 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
     implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.1.0")
 
     implementation("org.slf4j:slf4j-api:2.0.9")
