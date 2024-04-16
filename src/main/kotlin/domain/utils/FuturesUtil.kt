2107f54bf287c63acab4e1b18202d99a1f6a9ecb
KimA-Mik <ananim38@gmail.com>
2024-04-16 15:50:28 +0300

    implement getFutureExpirationYear for short codes

--- src/main/kotlin/domain/utils/FuturesUtil.kt
+++ src/main/kotlin/domain/utils/FuturesUtil.kt
@@ -1,5 +1,8 @@
 package domain.utils
 
+import kotlinx.datetime.Clock
+import kotlinx.datetime.toLocalDateTime
+
 object FuturesUtil {
     private val wrongDay = DayOfYear(-1, -1)
     val codeToMonths =
@@ -20,16 +23,40 @@ object FuturesUtil {
 
     fun getFutureExpireDay(ticker: String): DayOfYear {
         return when (ticker.length) {
-            4 -> parseShortFutureCode(ticker)
+            4 -> parseShortFutureExpirationDay(ticker)
             else -> wrongDay
         }
     }
 
-    private fun parseShortFutureCode(ticker: String): DayOfYear {
+    fun getFutureExpirationYear(
+        ticker: String,
+        currentYear: Int =
+            Clock.System.now()
+                .toLocalDateTime(DateUtil.timezoneMoscow).year
+    ): Int {
+        return when (ticker.length) {
+            4 -> parseShortFutureExpirationYear(ticker, currentYear)
+            else -> -1
+        }
+    }
+
+    private fun parseShortFutureExpirationDay(
+        ticker: String,
+    ): DayOfYear {
         val code = ticker[2]
         val month = codeToMonths[code] ?: return wrongDay
 
-
         return DayOfYear(21, month)
+    }
+
+    private fun parseShortFutureExpirationYear(
+        ticker: String,
+        currentYear: Int
+    ): Int {
+        val expirationYear = ticker.last().digitToIntOrNull() ?: return -1
+        val current = currentYear % 10
+        if (expirationYear == current) return currentYear
+        if (expirationYear > current) return currentYear - current + expirationYear
+        return currentYear - current + 10 + expirationYear
     }
 }
