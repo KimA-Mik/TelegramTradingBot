c8fe71bd49919e61b79ec1cb1fd3c6b21a772f26
KimA-Mik <ananim38@gmail.com>
2024-04-29 17:47:29 +0300

    Bump org.xerial:sqlite-jdbc to 3.45.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
 
-    implementation("org.xerial:sqlite-jdbc:3.45.2.0")
+    implementation("org.xerial:sqlite-jdbc:3.45.3.0")
 
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0-RC.2")
 }
