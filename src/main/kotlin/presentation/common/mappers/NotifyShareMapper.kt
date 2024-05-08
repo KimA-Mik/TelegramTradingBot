623a8471debfa8a323f5b3f37214eb2cff355562
KimA-Mik <ananim38@gmail.com>
2024-05-08 21:54:20 +0300

    Factor out NotifyShare to text converters

---
+++ src/main/kotlin/presentation/common/mappers/NotifyShareMapper.kt
@@ -0,0 +1,42 @@
+package presentation.common.mappers
+
+import domain.updateService.model.NotifyShare
+import domain.utils.DateUtil
+import kotlinx.datetime.format
+import kotlinx.datetime.toLocalDateTime
+import presentation.common.MarkdownUtil
+import presentation.common.TinInvestUtil
+import presentation.common.formatAndTrim
+import presentation.common.futureDateFormat
+import presentation.telegram.common.ROUBLE_SIGN
+
+
+fun NotifyShare.toUpdateText(): String {
+    var res = String()
+    val inlineShareUrl = MarkdownUtil.inlineUrl(
+        text = shareTicker,
+        url = TinInvestUtil.shareUrl(shareTicker)
+    )
+    res += "$inlineShareUrl: ${sharePrice.formatAndTrim(2)}$ROUBLE_SIGN"
+
+    futures.forEach { future ->
+        val date = future.expirationDate.toLocalDateTime(DateUtil.timezoneMoscow).format(futureDateFormat)
+        val inlineFutureUrl = MarkdownUtil.inlineUrl(
+            text = future.ticker,
+            url = TinInvestUtil.futureUrl(future.ticker)
+        )
+        res += "\n$date - $inlineFutureUrl: ${future.price.formatAndTrim(2)}$ROUBLE_SIGN, " +
+                "${future.annualPercent.formatAndTrim(2)}% годовых, " +
+                "после налога ${future.annualAfterTaxes.formatAndTrim(2)}%"
+    }
+
+    return res
+}
+
+fun NotifyShare.toInsufficientUpdateText(): String {
+    val shareInlineUrl = MarkdownUtil.inlineUrl(
+        text = shareTicker,
+        url = TinInvestUtil.shareUrl(shareTicker)
+    )
+    return "Для $shareInlineUrl процент годовых стал меньше, чем ${minimalDifference.formatAndTrim(2)}%"
+}
