5262e03ba527b47d1d8138513c61431d2ef98677
KimA-Mik <ananim38@gmail.com>
2024-05-29 15:05:15 +0300

    bump coroutines to 1.8.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
     implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.1.0")
 
     implementation("org.slf4j:slf4j-simple:2.0.13")
@@ -31,7 +31,7 @@ dependencies {
 
     implementation("org.xerial:sqlite-jdbc:3.45.3.0")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0-RC.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
 }
 
 tasks.test {
