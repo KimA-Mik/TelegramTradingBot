97c874024cb41ec49fdd2d7e48a44861381aed22
KimA-Mik <ananim38@gmail.com>
2024-05-29 16:07:11 +0300

    Implement calculateRsi

--- src/main/kotlin/domain/math/MathUtil.kt
+++ src/main/kotlin/domain/math/MathUtil.kt
@@ -1,7 +1,7 @@
 package domain.math
 
 object MathUtil {
-    fun calculateEma(values: DoubleArray): DoubleArray {
+    fun calculateEmas(values: DoubleArray): DoubleArray {
         val count = values.size
         val res = DoubleArray(values.size)
         if (count == 0) {
@@ -15,5 +15,47 @@ object MathUtil {
         }
 
         return res
+    }
+
+    fun calculateEma(values: DoubleArray): Double {
+        val count = values.size
+        var res = 0.0
+        if (count == 0) {
+            return res
+        }
+        res = values.first()
+
+        val weight = 2.0 / (count + 1.0)
+        for (i in 1 until count) {
+            res = values[i] * weight + res * (1 - weight)
+        }
+
+        return res
+    }
+
+    fun calculateRsi(prices: DoubleArray): Double {
+        val size = prices.size
+        if (size < 2) {
+            return 0.0
+        }
+
+        val ups = DoubleArray(size - 1)
+        val downs = DoubleArray(size - 1)
+
+        for (i in 1 until size) {
+            val diff = prices[i] - prices[i - 1]
+            if (diff > 0.0) {
+                ups[i - 1] = diff
+            } else {
+                downs[i - 1] = -diff
+            }
+        }
+
+        val upsEma = calculateEma(ups)
+        val downsEma = calculateEma(downs)
+
+        val rs = upsEma / downsEma
+        val rsi = 100.0 - (100.0 / (1.0 + rs))
+        return rsi
     }
 }
--- src/test/kotlin/domain/math/MathUtilTest.kt
+++ src/test/kotlin/domain/math/MathUtilTest.kt
@@ -1,34 +1,44 @@
 package domain.math
 
 import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class MathUtilTest {
 
     @Test
-    fun calculateEma() {
+    fun calculateEmas() {
         val values = doubleArrayOf(15.0, 11.0, 10.0)
-        val res = MathUtil.calculateEma(values)
+        val res = MathUtil.calculateEmas(values)
 
         val expected = doubleArrayOf(15.0, 13.0, 11.5)
         assertArrayEquals(expected, res, 0.001)
     }
 
     @Test
-    fun calculateEmaEmpty() {
+    fun calculateEmasEmpty() {
         val values = doubleArrayOf()
-        val res = MathUtil.calculateEma(values)
+        val res = MathUtil.calculateEmas(values)
 
         val expected = doubleArrayOf()
         assertArrayEquals(expected, res, 0.001)
     }
 
     @Test
-    fun calculateEmaSingle() {
+    fun calculateEmasSingle() {
         val values = doubleArrayOf(123.0)
-        val res = MathUtil.calculateEma(values)
+        val res = MathUtil.calculateEmas(values)
 
         val expected = doubleArrayOf(123.0)
         assertArrayEquals(expected, res, 0.001)
+    }
+
+    @Test
+    fun calculateEma() {
+        val values = doubleArrayOf(15.0, 11.0, 10.0)
+        val res = MathUtil.calculateEma(values)
+
+        val expected = 11.5
+        assertEquals(expected, res, 0.001)
     }
 }
