9a52d962821fa4b312d174effbd67f28609878b0
KimA-Mik <ananim38@gmail.com>
2024-05-31 15:37:33 +0300

    Implement TradingView ris

--- src/main/kotlin/domain/math/MathUtil.kt
+++ src/main/kotlin/domain/math/MathUtil.kt
@@ -1,60 +1,41 @@
 package domain.math
 
 object MathUtil {
-    fun calculateEmas(values: DoubleArray): DoubleArray {
-        val count = values.size
-        val res = DoubleArray(values.size)
-        if (count == 0) {
-            return res
-        }
-        res[0] = values[0]
-
-        val weight = 2.0 / (count + 1.0)
-        for (i in 1 until count) {
-            res[i] = values[i] * weight + res[i - 1] * (1 - weight)
-        }
-
-        return res
-    }
-
-    fun calculateEma(values: DoubleArray): Double {
-        val count = values.size
-        var res = 0.0
-        if (count == 0) {
-            return res
-        }
-        res = values.first()
-
-        val weight = 2.0 / (count + 1.0)
-        for (i in 1 until count) {
-            res = values[i] * weight + res * (1 - weight)
-        }
-
-        return res
-    }
-
-    fun calculateRsi(prices: DoubleArray): Double {
-        val size = prices.size
-        if (size < 2) {
+    //https://stackoverflow.com/questions/69980426/how-to-get-the-same-rsi-as-tradingview-in-java
+    fun calculateRma(values: DoubleArray, period: Int): Double {
+        if (values.isEmpty()) {
             return 0.0
         }
 
-        val ups = DoubleArray(size - 1)
-        val downs = DoubleArray(size - 1)
+        val alpha = 1.0 / period
+        val initialRma = values.average()
+        return values.fold(initialRma) { prevRma, value ->
+            alpha * value + (1.0 - alpha) * prevRma
+        }
+    }
 
-        for (i in 1 until size) {
+
+    fun calculateRsi(prices: DoubleArray, period: Int = 14): Double {
+        if (prices.isEmpty()) {
+            return 0.0
+        }
+
+        val ups = DoubleArray(prices.size - 1)
+        val downs = DoubleArray(prices.size - 1)
+
+        for (i in 1 until prices.size) {
             val diff = prices[i] - prices[i - 1]
-            if (diff > 0.0) {
+            if (diff >= 0.0) {
                 ups[i - 1] = diff
             } else {
                 downs[i - 1] = -diff
             }
         }
 
-        val upsEma = calculateEma(ups)
-        val downsEma = calculateEma(downs)
+        val upsRma = calculateRma(ups, period)
+        val downsRma = calculateRma(downs, period)
 
-        val rs = upsEma / downsEma
+        val rs = upsRma / downsRma
         val rsi = 100.0 - (100.0 / (1.0 + rs))
         return rsi
     }
