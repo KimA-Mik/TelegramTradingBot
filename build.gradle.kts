c356e342e2b0a8352ebd81a09cb620f62594e31c
KimA-Mik <ananim38@gmail.com>
2025-01-09 03:34:35 +0300

    Big bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.0.0"
+    kotlin("jvm") version "2.1.0"
     application
 }
 
@@ -13,17 +13,17 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
-    implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.1.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+    implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.2.0")
 
-    implementation("org.slf4j:slf4j-simple:2.0.13")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
 
-    implementation("ru.tinkoff.piapi:java-sdk-core:1.5")
+    implementation("ru.tinkoff.piapi:java-sdk-core:1.27")
     implementation("ru.mail.im:bot-api:1.2.3")
 
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
-    val koinVersion = "3.5.6"
+    val koinVersion = "4.0.1"
     implementation("io.insert-koin:koin-core:$koinVersion")
 
     val exposedVersion: String by project
@@ -31,9 +31,9 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
 
-    implementation("org.xerial:sqlite-jdbc:3.46.0.0")
+    implementation("org.xerial:sqlite-jdbc:3.47.2.0")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
 
     val ta4jVersion: String by project
     implementation("org.ta4j:ta4j-core:$ta4jVersion")
