6809e2228e5d2fbb83e2120ed0cece513c83aa1c
KimA-Mik <ananim38@gmail.com>
2025-10-28 20:00:25 +0300

    Fix embarrassing bug in QuotationMapper

--- FuturesBot/src/main/kotlin/data/tinkoff/mappers/QuotationMapper.kt
+++ FuturesBot/src/main/kotlin/data/tinkoff/mappers/QuotationMapper.kt
@@ -1,15 +1,7 @@
 package data.tinkoff.mappers
 
 import ru.tinkoff.piapi.contract.v1.Quotation
-import kotlin.math.abs
 
 fun Quotation.toDouble(): Double {
-    val high = units.toDouble()
-    val low = if (nano != 0) {
-        "0.${abs(nano)}".toDouble()
-    } else {
-        0.0
-    }
-
-    return high + low
+    return units + nano / 1_000_000_000.0
 }
