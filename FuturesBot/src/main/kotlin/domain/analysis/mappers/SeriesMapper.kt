5bbfa1e678c09278694ad6f3f92e714e738ed30f
KimA-Mik <ananim38@gmail.com>
2025-11-26 19:27:22 +0300

    Fix futures bot bug casued by ta4j update. it's almost time to factor out common things

--- FuturesBot/src/main/kotlin/domain/analysis/mappers/SeriesMapper.kt
+++ FuturesBot/src/main/kotlin/domain/analysis/mappers/SeriesMapper.kt
@@ -4,13 +4,16 @@ import domain.tinkoff.model.TinkoffCandle
 import org.ta4j.core.BarSeries
 import org.ta4j.core.BaseBarSeriesBuilder
 import org.ta4j.core.bars.TimeBarBuilder
+import org.ta4j.core.num.DoubleNumFactory
 import kotlin.time.ExperimentalTime
 import kotlin.time.toJavaDuration
 import kotlin.time.toJavaInstant
 
 @OptIn(ExperimentalTime::class)
 fun List<TinkoffCandle>.toSeries(seriesName: String = String()): BarSeries {
-    val series = BaseBarSeriesBuilder().withName(seriesName).build()
+    val series = BaseBarSeriesBuilder()
+        .withNumFactory(DoubleNumFactory.getInstance())
+        .withName(seriesName).build()
     this.forEach { candle ->
         val bar = TimeBarBuilder()
             .timePeriod(candle.duration.toJavaDuration())
@@ -19,7 +22,7 @@ fun List<TinkoffCandle>.toSeries(seriesName: String = String()): BarSeries {
             .highPrice(candle.high)
             .lowPrice(candle.low)
             .closePrice(candle.close)
-            //.volume()
+            .volume(candle.volume)
             .build()
 
         series.addBar(bar)
