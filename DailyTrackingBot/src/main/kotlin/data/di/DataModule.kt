4bb7ee7da491c667d50d07324af61aa18edbcee4
KimA-Mik <ananim38@gmail.com>
2025-12-15 11:45:29 +0300

    Set CacheServerApi log level to NONE

--- DailyTrackingBot/src/main/kotlin/data/di/DataModule.kt
+++ DailyTrackingBot/src/main/kotlin/data/di/DataModule.kt
@@ -3,6 +3,7 @@ package data.di
 import data.db.DatabaseConnector
 import data.db.UserRepositoryImpl
 import domain.user.repository.UserRepository
+import io.ktor.client.plugins.logging.*
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
@@ -11,5 +12,5 @@ import ru.kima.cacheserver.api.api.CacheServerApi
 fun dataModule(cacheServerApiUrl: String) = module {
     singleOf(::DatabaseConnector)
     singleOf(::UserRepositoryImpl) bind UserRepository::class
-    single { CacheServerApi(cacheServerApiUrl) }
+    single { CacheServerApi(cacheServerApiUrl, logLevel = LogLevel.NONE) }
 }
